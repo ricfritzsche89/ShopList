c2066772479029e87f035bf02ace80e9fd7abcc4
ricfritzsche89 <ricfritzsche@gmail.com>
2026-08-31 13:35:13 +0200

    Updates

--- sw.js
+++ sw.js
@@ -2,8 +2,18 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          console.log('Lösche alten Cache:', key);
+          return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 self.addEventListener('fetch', (e) => {
-  // Minimaler Service Worker für PWA-Kriterien
+  // Netzwerk-First-Strategie (kein lokaler Cache für JS/HTML-Bundles)
+  e.respondWith(fetch(e.request));
 });
